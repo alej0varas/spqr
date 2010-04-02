647d475726f5273c988f97521172edf68a472f89
maximinus <maximinus@2be61f21-b1ab-43f6-960c-aab46cf39629>
2010-04-02 13:32:48 +0000

    Updated all code to note license is GPL3 or later.
    git-svn-id: https://spqr.svn.sourceforge.net/svnroot/spqr@16 2be61f21-b1ab-43f6-960c-aab46cf39629

--- scripts/spqr_console.py
+++ scripts/spqr_console.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python
 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 # this file contains a class that contains all of the functions
 # used by the spqr console. Each function has to return a text
 # string that is then output by the console
--- spqr.py
+++ spqr.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-# SPQR source code, Copyright 2005-2007 Chris Smith
+# SPQR source code, Copyright 2005-2010 Chris Smith
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 
 # This program is distributed in the hope that it will be useful,
