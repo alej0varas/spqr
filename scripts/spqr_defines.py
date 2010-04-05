a773136fd7b7466d6570fd719924b8bf42e8b3c6
maximinus <maximinus@2be61f21-b1ab-43f6-960c-aab46cf39629>
2010-04-05 12:55:37 +0000

    Updated TODO list (with history). Minor adjustments to spqr_defines.py.
    git-svn-id: https://spqr.svn.sourceforge.net/svnroot/spqr@24 2be61f21-b1ab-43f6-960c-aab46cf39629

--- scripts/spqr_defines.py
+++ scripts/spqr_defines.py
@@ -22,14 +22,14 @@ import pygame.locals as PYGAME
 # as they are, unless you really know what you are doing. Probably the
 # most useful when debugging is SPQR_FULLSCREEN
 
-VERSION				= "v0.3.54"
+VERSION				= "v0.3.55"
 AUTHOR				= "Chris Smith"
 EMAIL				= "maximinus@gmail.com"
 SYSTEM				= "GNU/Linux"
 WEBSITE				= "http://sourceforge.net/projects/spqr/"
 STARTED				= "1st Jan 2005"
-LAST_UPDATE			= "2nd April 2010"
-CODELINES			= "8428"
+LAST_UPDATE			= "5th April 2010"
+CODELINES			= "8509"
 
 # before we go any further, this one is a must ;-)
 # currently adds debug menu bar , along with access to
