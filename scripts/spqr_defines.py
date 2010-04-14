2cb468f77d8053bca881db6be2c2e3a95a3980fe
maximinus <maximinus@2be61f21-b1ab-43f6-960c-aab46cf39629>
2010-04-14 13:13:25 +0000

    Added toeholds map graphics. Updated main scenario file to account for this.
    git-svn-id: https://spqr.svn.sourceforge.net/svnroot/spqr@31 2be61f21-b1ab-43f6-960c-aab46cf39629

--- scripts/spqr_defines.py
+++ scripts/spqr_defines.py
@@ -22,14 +22,14 @@ import pygame.locals as PYGAME
 # as they are, unless you really know what you are doing. Probably the
 # most useful when debugging is SPQR_FULLSCREEN
 
-VERSION				= "v0.3.56"
+VERSION				= "v0.3.57"
 AUTHOR				= "Chris Smith"
 EMAIL				= "maximinus@gmail.com"
 SYSTEM				= "GNU/Linux"
 WEBSITE				= "http://sourceforge.net/projects/spqr/"
 STARTED				= "1st Jan 2005"
-LAST_UPDATE			= "10th April 2010"
-CODELINES			= "8499"
+LAST_UPDATE			= "14th April 2010"
+CODELINES			= "8609"
 
 # before we go any further, this one is a must ;-)
 # currently adds debug menu bar , along with access to
@@ -137,8 +137,8 @@ TXT_MIN_LG			= 20
 PAN_RATIO			= 1
 GOLDEN_RATIO		= 1.618
 # location of Rome on main (graphical) map
-ROME_XPOS			= 850
-ROME_YPOS			= 799
+ROME_XPOS			= 1096
+ROME_YPOS			= 824
 # maximum number of units on any hex
 # despite this value, making it MORE than 4 will break the game
 MAX_STACKING		= 4
