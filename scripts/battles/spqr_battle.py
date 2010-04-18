de3a0e59c80aab7a431241c4da862254e5c236e7
maximinus <maximinus@2be61f21-b1ab-43f6-960c-aab46cf39629>
2010-04-18 14:07:42 +0000

    On a battle, extracts correct battle text. This is a clever as it gets for battles for 4.0.
    git-svn-id: https://spqr.svn.sourceforge.net/svnroot/spqr@35 2be61f21-b1ab-43f6-960c-aab46cf39629

--- scripts/battles/spqr_battle.py
+++ scripts/battles/spqr_battle.py
@@ -22,6 +22,7 @@ from .. import spqr_defines as SPQR
 from .. import spqr_widgets as SWIDGET
 from .. import spqr_events as SEVENT
 from ..data import spqr_battle_text as BTEXT
+import random as RAND
 
 def test(lgui,attack,defend):
 	"""Routine to test whatever the latest version of the window
@@ -41,9 +42,17 @@ def test(lgui,attack,defend):
 		return(True)
 
 	# get the various texts:
-	atext=[]
-	btext=[]
-	
+	print "\nNew:\n"
+	atext,dtext=getTexts(BTEXT.ground,1,3)
+	a2,d2=getTexts(BTEXT.change,1,4)
+	a3,d3=getTexts(BTEXT.morale,0,2)
+	atext.extend(a2)
+	atext.extend(a3)
+	dtext.extend(d2)
+	dtext.extend(d3)
+	print atext
+	print dtext
+
 	# now we can start to build up the window. This is a complex one.
 	# from the top, we must show the units, then a message showing both
 	# enemy state and your attack options; then the status of both
@@ -68,8 +77,27 @@ def test(lgui,attack,defend):
 	lgui.addDirtyRect(win_img,lgui.windows[index].rect)
 	return(False)
 
-def getAttackersText():
-	pass
+def getTexts(sample,min_texts,max_texts):
+	"""Return the data for the ground attack texts
+	   This is a list of 2 lists, the first being the attacker ones
+	   And the second being the defender ones"""
+	# we need a certain number 1-5 of texts:
+	total=RAND.sample(sample,RAND.randint(min_texts,max_texts))
+	# fill a new list with these elements
+	attack_text=[]
+	defend_text=[]
+	for i in total:
+		choice=RAND.randint(0,3)
+		if(choice==0):
+			attack_text.append(i["attack_pro"])
+		elif(choice==1):
+		 	attack_text.append(i["attack_con"])
+		elif(choice==2):
+		 	defend_text.append(i["defend_pro"])
+		else:
+		 	defend_text.append(i["defend_con"])
+	# strip unwanted ones
+	atext=[i for i in attack_text if i[0]!=""]
+	dtext=[i for i in defend_text if i[0]!=""]
+	return(atext,dtext)
 
-def getDefendersText():
-	pass
